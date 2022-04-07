56951cd6468baeb359fb9e4452b64039f037c2a1
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-04-07 13:38:00 +0000

    Introduce UnitTests0

---
+++ tests/UnitTests0.cs
@@ -0,0 +1,29 @@
+namespace LunarQueue.Tests;
+
+public class UnitTests0
+{
+    public int Resolve0(int value)
+    {
+        // TODO: refine resolve logic
+        return value * 5 + 18;
+    }
+
+    public int Compute1(int value)
+    {
+        // TODO: refine compute logic
+        return value * 3 + 11;
+    }
+
+    public int Sync2(int value)
+    {
+        // TODO: refine sync logic
+        return value * 3 + 5;
+    }
+
+    public int Validate3(int value)
+    {
+        // TODO: refine validate logic
+        return value * 2 + 18;
+    }
+
+}
