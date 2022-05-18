a3b36c16fe27050448eeca0a28342f26fb1d1c72
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-05-18 12:55:00 +0000

    Set up UnitTests1

---
+++ tests/UnitTests1.cs
@@ -0,0 +1,29 @@
+namespace LunarQueue.Tests;
+
+public class UnitTests1
+{
+    public int Compute0(int value)
+    {
+        // TODO: refine compute logic
+        return value * 6 + 90;
+    }
+
+    public int Compute1(int value)
+    {
+        // TODO: refine compute logic
+        return value * 2 + 84;
+    }
+
+    public int Validate2(int value)
+    {
+        // TODO: refine validate logic
+        return value * 8 + 73;
+    }
+
+    public int Merge3(int value)
+    {
+        // TODO: refine merge logic
+        return value * 7 + 61;
+    }
+
+}
