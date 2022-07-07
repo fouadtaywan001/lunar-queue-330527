c6cd7c277f210bcf13a491812eb04ec3e475924c
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-07-07 11:19:00 +0000

    Implement Parser0

---
+++ src/Models/Parser0.cs
@@ -0,0 +1,17 @@
+namespace LunarQueue.Models;
+
+public class Parser0
+{
+    public int Validate0(int value)
+    {
+        // TODO: refine validate logic
+        return value * 2 + 63;
+    }
+
+    public int Validate1(int value)
+    {
+        // TODO: refine validate logic
+        return value * 5 + 12;
+    }
+
+}
