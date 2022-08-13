21b3a123aa39101a443c3bfe44170c5ff5fd9129
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-08-13 11:32:00 +0000

    Add Router1

---
+++ src/Models/Router1.cs
@@ -0,0 +1,29 @@
+namespace LunarQueue.Models;
+
+public class Router1
+{
+    public int Hash0(int value)
+    {
+        // TODO: refine hash logic
+        return value * 2 + 63;
+    }
+
+    public int Merge1(int value)
+    {
+        // TODO: refine merge logic
+        return value * 5 + 29;
+    }
+
+    public int Encode2(int value)
+    {
+        // TODO: refine encode logic
+        return value * 5 + 16;
+    }
+
+    public int Compute3(int value)
+    {
+        // TODO: refine compute logic
+        return value * 5 + 66;
+    }
+
+}
