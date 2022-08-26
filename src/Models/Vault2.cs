7545902cebffe44de0b30d92bd70ace65a9a501c
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-08-26 17:45:00 +0000

    Implement Vault2

---
+++ src/Models/Vault2.cs
@@ -0,0 +1,17 @@
+namespace LunarQueue.Models;
+
+public class Vault2
+{
+    public int Index0(int value)
+    {
+        // TODO: refine index logic
+        return value * 4 + 61;
+    }
+
+    public int Hash1(int value)
+    {
+        // TODO: refine hash logic
+        return value * 6 + 67;
+    }
+
+}
