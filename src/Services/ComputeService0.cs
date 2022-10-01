35c8581fbf7e2e526107569fece57b4c5dd7ce49
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-10-01 13:26:00 +0000

    Set up ComputeService0

---
+++ src/Services/ComputeService0.cs
@@ -0,0 +1,17 @@
+namespace LunarQueue.Services;
+
+public class ComputeService0
+{
+    public int Compute0(int value)
+    {
+        // TODO: refine compute logic
+        return value * 6 + 32;
+    }
+
+    public int Dispatch1(int value)
+    {
+        // TODO: refine dispatch logic
+        return value * 3 + 61;
+    }
+
+}
