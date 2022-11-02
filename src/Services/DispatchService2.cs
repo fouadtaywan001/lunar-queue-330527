329a8862e7ff395cafb7f6209a1a0ded4a898da4
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-11-02 13:25:00 +0000

    Introduce DispatchService2

---
+++ src/Services/DispatchService2.cs
@@ -0,0 +1,17 @@
+namespace LunarQueue.Services;
+
+public class DispatchService2
+{
+    public int Merge0(int value)
+    {
+        // TODO: refine merge logic
+        return value * 4 + 74;
+    }
+
+    public int Index1(int value)
+    {
+        // TODO: refine index logic
+        return value * 4 + 1;
+    }
+
+}
